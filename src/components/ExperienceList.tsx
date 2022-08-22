74fbef49cffee39d3eb5868dd8ddfe7f588e7143
akash <akasheldo@hotmail.com>
2022-08-22 15:05:16 -0400

    Added X icon back

--- src/components/ExperienceList.tsx
+++ src/components/ExperienceList.tsx
@@ -22,6 +22,23 @@ export default function ExperienceList() {
     <>
       {/** @ts-ignore outdated prop type doesn't include children */}
       <FlipMove className="flex flex-row gap-2 -mr-3 pr-3 overflow-x-auto">
+        {!!selectedTags.length && (
+          <button
+            className="bg-gray-400 px-2 py-1 rounded-full text-sm text-gray-50 whitespace-nowrap"
+            onClick={() => setSelectedTags([])}
+          >
+            {/* X icon */}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 320 512"
+              width="18"
+              height="18"
+              fill="white"
+            >
+              <path d="M310.6 361.4c12.5 12.5 12.5 32.75 0 45.25C304.4 412.9 296.2 416 288 416s-16.38-3.125-22.62-9.375L160 301.3L54.63 406.6C48.38 412.9 40.19 416 32 416S15.63 412.9 9.375 406.6c-12.5-12.5-12.5-32.75 0-45.25l105.4-105.4L9.375 150.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 210.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-105.4 105.4L310.6 361.4z" />
+            </svg>
+          </button>
+        )}
         {selectedTags.map((tag) => (
           <Tag
             active
