2628503b801364c6616c63911f5d4eab8df819f1
akash <akasheldo@hotmail.com>
2022-08-22 14:43:54 -0400

    with css

--- src/components/ExperienceList.tsx
+++ src/components/ExperienceList.tsx
@@ -1,9 +1,15 @@
 import { useState } from "react";
-import { CSSTransition, TransitionGroup } from "react-transition-group";
+import {
+  CSSTransition,
+  SwitchTransition,
+  TransitionGroup,
+} from "react-transition-group";
 import cards from "../cards";
 import Card from "./Card";
 import Tag from "./Tag";
+import "./styles.css";
 
+// pick item, remaining fade out, picked slides to join selected, remainingTags fade in
 export default function ExperienceList() {
   const [selectedTags, setSelectedTags] = useState<string[]>([]);
 
@@ -18,70 +24,49 @@ export default function ExperienceList() {
     ),
   ].filter((tag) => !selectedTags.includes(tag));
 
-  const timeout = 500;
-  const classNames = {
-    enter: "opacity-0",
-    enterActive: "opacity-100",
-    exit: "opacity-100",
-    exitDone: "opacity-0",
-  };
+  const timeout = 700;
+  const classNames = "item";
 
   return (
     <>
-      <TransitionGroup className="flex flex-row gap-2 -mr-3 pr-3 overflow-x-auto">
-        {selectedTags.length > 0 && (
-          <CSSTransition
-            classNames={classNames}
-            timeout={timeout}
-            addEndListener={() => {}}
-          >
-            <button
-              className="transition-opacity ease-in-out bg-gray-400 px-1 py-1 rounded-full text-sm text-gray-50 whitespace-nowrap"
-              onClick={() => setSelectedTags([])}
+      <div className="flex flex-row gap-2 -mr-3 pr-3 overflow-x-auto">
+        <TransitionGroup component={null}>
+          {selectedTags.map((tag) => (
+            <CSSTransition
+              key={tag + "-s"}
+              classNames={classNames}
+              timeout={timeout}
+              addEndListener={() => {}}
             >
-              {/* X icon */}
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 320 512"
-                width="18"
-                height="18"
-                fill="white"
+              <Tag
+                active
+                onClick={() =>
+                  setSelectedTags(selectedTags.filter((t) => t !== tag))
+                }
               >
-                <path d="M310.6 361.4c12.5 12.5 12.5 32.75 0 45.25C304.4 412.9 296.2 416 288 416s-16.38-3.125-22.62-9.375L160 301.3L54.63 406.6C48.38 412.9 40.19 416 32 416S15.63 412.9 9.375 406.6c-12.5-12.5-12.5-32.75 0-45.25l105.4-105.4L9.375 150.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 210.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-105.4 105.4L310.6 361.4z" />
-              </svg>
-            </button>
-          </CSSTransition>
-        )}
-        {[...selectedTags].map((tag) => (
-          <CSSTransition
-            key={tag + "-s"}
-            classNames={classNames}
-            timeout={timeout}
-            addEndListener={() => {}}
-          >
-            <Tag
-              active
-              onClick={() =>
-                setSelectedTags(selectedTags.filter((val) => val !== tag))
-              }
+                {tag}
+              </Tag>
+            </CSSTransition>
+          ))}
+
+          {remainingTags.map((tag) => (
+            <CSSTransition
+              key={tag + "-r"}
+              classNames={classNames}
+              timeout={timeout}
+              addEndListener={() => {}}
             >
-              {tag}
-            </Tag>
-          </CSSTransition>
-        ))}
-        {remainingTags.map((tag) => (
-          <CSSTransition
-            key={tag + "-r"}
-            classNames={classNames}
-            timeout={timeout}
-            addEndListener={() => {}}
-          >
-            <Tag onClick={() => setSelectedTags([...selectedTags, tag])}>
-              {tag}
-            </Tag>
-          </CSSTransition>
-        ))}
-      </TransitionGroup>
+              <Tag
+                key={tag + "-r"}
+                onClick={() => setSelectedTags([...selectedTags, tag])}
+              >
+                {tag}
+              </Tag>
+            </CSSTransition>
+          ))}
+        </TransitionGroup>
+      </div>
+
       <div className="pt-4 flex flex-col gap-4">
         {experiences.map((c) => (
           <Card key={c.title} card={c} />
