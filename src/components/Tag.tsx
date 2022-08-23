c52281c652229f0befba6f5557965a050ac81ae9
akash <akasheldo@hotmail.com>
2022-08-23 12:49:51 -0400

    Added hover states to everything

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -9,11 +9,16 @@ export interface TagProps {
 const Tag = forwardRef(({ active, onClick, children }: TagProps, ref) => {
   const Element = onClick ? "button" : "div";
   const bgColor = active ? "bg-gray-400" : "bg-gray-500";
+  const hover = onClick
+    ? active
+      ? "hover:bg-gray-300 hover:text-gray-600"
+      : "hover:bg-gray-400"
+    : "";
 
   return (
     <Element
       ref={ref as any}
-      className={`${bgColor} px-2 py-1 rounded-full text-sm text-gray-50 whitespace-nowrap`}
+      className={`${bgColor} ${hover} group px-2 py-1 rounded-full text-sm text-gray-50 whitespace-nowrap`}
       onClick={onClick}
     >
       {children}
